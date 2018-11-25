85e66a5f2fe6afc5cb8682fb20684f8f7b86c688
SavanMalhotra <43863307+SavanMalhotra@users.noreply.github.com>
2018-11-25 11:51:45 -0500

    Update test.ino

--- test.ino
+++ test.ino
@@ -1,7 +1,21 @@
+//this program turns a motor on for 5 seconds and then turns it off for 1 second anc continues in a loop
+
+int motorOne = 9;
+
 void setup() {
-  print("hello world");
+ pinMode(motorOne, OUTPUT);
+}
+void loop() {
+ on(5000, motorOne);
+ off(1000, motorOne);
 }
 
-void loop() {
-    
+void on(int duration, int pin){
+ digitalWrite(pin, LOW);
+ delay(duration);
+}
+ 
+void off(int duration, int pin){
+ digitalWrite(pin, HIGH);
+ delay(duration);
 }
