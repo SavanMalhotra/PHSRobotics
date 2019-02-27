fc2e34a8212d1169328a8482af42cd5c86e67c7c
Savan Malhotra <43863307+SavanMalhotra@users.noreply.github.com>
2019-02-27 18:34:48 -0500

    Update multiSensors.ino

--- multiSensors.ino
+++ multiSensors.ino
@@ -1,6 +1,6 @@
 #include <Wire.h>
-char x;//holds variable to be sent to other arduino
-bool allowedToExecute = true;
+char x;//holds character that will be sent to other arduino
+bool allowedToExecute = true;//used to determine if an object has already been detected or not
 
 int trigPin = 2;//trigger pin for all sensors
 
@@ -22,35 +22,40 @@ void setup() {
 
 void loop() {
   long duration1, distance1;
-  digitalWrite(trigPin, LOW);  // Added this line
-  delayMicroseconds(2); // Added this line
+  digitalWrite(trigPin, LOW);  
+  delayMicroseconds(2); 
   digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10); // Added this line
+  delayMicroseconds(10); 
   digitalWrite(trigPin, LOW);
   duration1 = pulseIn(echoPin1, HIGH);
   distance1 = (duration1/2) / 29.1;
-  Serial.print ( "Sensor1  ");
-  Serial.print ( distance1);
+  Serial.print ("Sensor1  ");
+  Serial.print (distance1);
   Serial.println("cm");
   if(distance1 <= 20 && allowedToExecute) {
     Serial.println("a was sent??");
     sendEvent('A', 9);
     Serial.println("a was sent??");  
   }
-  delay(20);
+  delay(20);//delay added to allow code to process and reset
   
   long duration2, distance2;
-  digitalWrite(trigPin, LOW);  // Added this line
-  delayMicroseconds(2); // Added this line
+  digitalWrite(trigPin, LOW); 
+  delayMicroseconds(2);
   digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10); // Added this line
+  delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
   duration2 = pulseIn(echoPin2, HIGH);
   distance2= (duration2/2) / 29.1;
   Serial.print("Sensor2  ");
   Serial.print(distance2);
   Serial.println("cm");
-  delay(20);
+  if(distance2 <= 20 && allowedToExecute) {
+    Serial.println("a was sent??");
+    sendEvent('A', 9);
+    Serial.println("a was sent??");  
+  }
+  delay(20);//delay added to allow code to process and reset
 }
 
 void receiveEvent(int bytes) {
@@ -62,7 +67,6 @@ void receiveEvent(int bytes) {
 }
 
 void sendEvent(char val, int id) {
-  
   Serial.println(val);
   allowedToExecute = false;
   Wire.beginTransmission(id);
@@ -70,5 +74,3 @@ void sendEvent(char val, int id) {
   Wire.endTransmission(id);
   Serial.println(val);
 }
-
-  
