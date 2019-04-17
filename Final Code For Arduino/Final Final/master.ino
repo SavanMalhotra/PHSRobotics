9d744eb98a967d9a4a4cdc306fbe91739bf485c5
Savan Malhotra <43863307+SavanMalhotra@users.noreply.github.com>
2019-04-17 18:04:38 -0400

    Create master.ino

---
+++ Final Code For Arduino/Final Final/master.ino
@@ -0,0 +1,132 @@
+#include <Wire.h>
+char x;
+bool allowedToExecute = true;
+int trigPin = 2;    // Trigger
+int LeftFrontProximity = 4;    // LeftFrontProximity Sensor echo pin
+int RightFrontProximity = 3;    // RightFrontProximity Sensor echo pin
+int TopFrontProximity = 5; //TopFrontProximity Sensor echo pin
+long LeftFrontDuration, LeftFrontCm, RightFrontDuration, RightFrontCm,TopFrontDuration, TopFrontCm;
+
+int proximityDistanceVariable = 20;
+String one = "";
+String two = "";
+
+int LC;
+int MC;
+
+int blockDistance;
+
+
+void setup() {
+  Wire.begin(9);//Used for conenction between arduinos
+  Wire.onReceive(receiveEvent);
+  pinMode(trigPin, OUTPUT);
+  pinMode(LeftFrontProximity, INPUT);
+  pinMode(RightFrontProximity, INPUT);
+  
+  Serial.begin(9600);
+}
+
+void receiveEvent(int bytes) {
+  x = Wire.read();
+  if(x == 'G') {
+    allowedToExecute = true;
+  }
+  if(x =='B'){
+    Serial.println("get B");
+    //checkBlock(); 
+    sendEvent('J', 9);
+  } 
+}
+
+void checkBlock() {
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(5);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+ 
+  // Read the signal from the sensor: a HIGH pulse whose
+  // duration is the time (in microseconds) from the sending
+  // of the ping to the reception of its echo off of an object.
+  pinMode(TopFrontProximity, INPUT);
+  TopFrontDuration = pulseIn(TopFrontProximity, HIGH);
+  // Convert the time into a distance
+ // blockDistance = (TopFrontDuration/2) / 29.1;     // Divide by 29.1 or multiply by 0.0343
+  blockDistance = 5;
+  delay(250);
+
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(5);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+ 
+  // Read the signal from the sensor: a HIGH pulse whose
+  // duration is the time (in microseconds) from the sending
+  // of the ping to the reception of its echo off of an object.
+  pinMode(TopFrontProximity, INPUT);
+  TopFrontDuration = pulseIn(TopFrontProximity, HIGH);
+  // Convert the time into a distance
+  //TopFrontCm = (TopFrontDuration/2) / 29.1;     // Divide by 29.1 or multiply by 0.0343
+  TopFrontCm = 5;
+  Serial.println("got block test");
+  if(TopFrontCm == blockDistance){
+    Serial.println("got block");
+    sendEvent('J', 9);
+  } 
+}
+
+void sendEvent(char val, int id) {
+  allowedToExecute = false;
+  Wire.beginTransmission(id);
+  Wire.write(val);
+  Wire.endTransmission(id);
+  Serial.println(val);
+}
+
+void loop() {
+  proximitySensor();
+  delay(300);
+}
+
+void proximitySensor() {
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(5);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+ 
+  // Read the signal from the sensor: a HIGH pulse whose
+  // duration is the time (in microseconds) from the sending
+  // of the ping to the reception of its echo off of an object.
+  pinMode(LeftFrontProximity, INPUT);
+  LeftFrontDuration = pulseIn(LeftFrontProximity, HIGH);
+  // Convert the time into a distance
+  LeftFrontCm = (LeftFrontDuration/2) / 29.1;     // Divide by 29.1 or multiply by 0.0343
+  
+  delayMicroseconds(5);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+ 
+  pinMode(RightFrontProximity, INPUT);
+  RightFrontDuration = pulseIn(RightFrontProximity, HIGH);
+  RightFrontCm = (RightFrontDuration/2) / 29.1;     // Divide by 29.1 or multiply by 0.0343
+ 
+  Serial.println(LeftFrontCm);
+  if(LeftFrontCm <= proximityDistanceVariable && allowedToExecute && LeftFrontCm >0) {
+    delay(475);
+    sendEvent('L', 9);
+  }
+
+  Serial.println(RightFrontCm);
+  if(RightFrontCm <= proximityDistanceVariable && allowedToExecute && RightFrontCm>0) {
+    delay(475);
+    sendEvent('R', 9);
+  }
+}
+
+void checkValues() {
+
+}
